49fc2a191fb11a27fdf5bb4fb427b6a1fda4c1ea
Joao Menezes <joao.menezes@ccc.ufcg.edu.br>
2018-10-05 19:20:52 -0300

    começo dos testes

--- app/routes/api/user.test.js
+++ app/routes/api/user.test.js
@@ -1,8 +1,9 @@
 var jest = require('jest');
 const request = require('supertest');
+const mongoose = require('mongoose');
 
 test('GET /user', ()=>{
-    
+
     return request("http://localhost:8080")
     .get('/api/users')
     .then(response => {
@@ -10,3 +11,36 @@ test('GET /user', ()=>{
     })
     .catch(fail);
 });
+
+test('POST /users', ()=>{
+  return request("http://localhost:8080")
+  .get('/api/users')
+  //.send({name:'João Menezes', email:'joao.menezes@ccc.ufcg.edu.br', password:'aaa', role:'aluno'})
+  .then(response => {
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe('João Menezes');
+    expect(response.body.email).toBe('joao.menezes@ccc.ufcg.edu.br');
+    expect(response.body.password).toBe('aaa');
+    expect(response.body.role).toBe('aluno');
+  })
+  .catch(fail)
+});
+
+// test('DELETE /users', ()=>{
+//   return request("http://localhost:8080")
+//   .delete('api/users')
+//   .send('5bb7ce1f7b6e7a0ef03e3feb')
+//   .then(response => {
+//     expect(response.status).toBe(200);
+//   })
+//   .catch(fail);
+// })
+//
+// test('GET /users/:id/getMyClasses', ()=>{
+//   return request("http://localhost:8080")
+//   .get('api/users/5baaccb973715e1f0d32f89e/getmyclasses')
+//   .then(response => {
+//       expect(response.status).toBe(200);
+//   })
+//   .catch(fail);
+// })
