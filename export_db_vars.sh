3383c5a1dc2cbac654640a88d83a177e205aa1cc
nicacioliveira <nnicacio@gmail.com>
2018-11-25 15:12:43 -0300

    env db variables

---
+++ export_db_vars.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Setting env variables up"
+export PORT=8080
+export APP_SECRET=miajuda
+export ENVIRONMENT=production
+export DBADDRESS=ds255282.mlab.com:55282
+export DBNAME=monitoriasufcg
+export DBCREDENTIALS=admin:admin123
+export AWS_ACCESS_KEY_ID=AKIAJGYI7VEQXC24KTDA
+export AWS_SECRET_ACCESS_KEY=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
+echo "Done"
