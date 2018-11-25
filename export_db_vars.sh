aca6eb48383a2ca1a85d0bb88e047cecbd511c94
Nicácio Oliveira de Sousa <nnicacio@gmail.com>
2018-11-25 17:06:10 -0300

    Delete export_db_vars.sh

--- export_db_vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Setting env variables up"
-export PORT=8080
-export APP_SECRET=miajuda
-export ENVIRONMENT=production
-export DBADDRESS=ds255282.mlab.com:55282
-export DBNAME=monitoriasufcg
-export DBCREDENTIALS=admin:admin123
-echo "Done"
